58bd339858d29cd95d16d26ad9a9df52d9bc740b
Kenneth sunjaya <kennethsunjaya@gmail.com>
2024-07-14 23:39:00 +0700

    fixed several bugs, added current track preview

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,16 @@ import SpotifyWebApi from 'spotify-web-api-node';
 import { Progress } from "flowbite-react";
 // import './App.css'
 
+interface Track {
+  name: string;
+  artists: any;
+  album: any;
+  thumbnail: string;
+}
+
 const App = () => {
-  const [audio, setAudio] = useState<any | null>(null);
+  const [audio, setAudio] = useState<any | null>(new Audio());
+  const [track, setTrack] = useState<Track | null>(null);
   const [paused, setPaused] = useState<boolean>(true);
   const [tracks, setTracks] = useState<any | null>([]);
   const [spotifyApi, setSpotifyApi] = useState<SpotifyWebApi | null>(null);
@@ -56,7 +64,6 @@ const App = () => {
   const handleKeyDown = async (event: any) => {
     if (event.key === "Enter" && !event.shiftKey && event.target.value.length > 0) {
       event.preventDefault();
-      console.log("pressed");
       await findTracks(event.target.value);
     }
   };
@@ -84,10 +91,11 @@ const App = () => {
     const newAudio = new Audio(datas);
     newAudio.volume = 0.2;
     setAudio(newAudio);
+    setPaused(false);
   };
 
   const handlePlayPause = () => {
-    if (audio) {
+    if (audio.duration > 0) {
       if (paused) {
         audio.play();
       } else {
@@ -98,15 +106,17 @@ const App = () => {
   };
 
   const formatTime = (seconds: number): string => {
+    if (!seconds) {
+      return '00:00';
+    }
     const minutes = Math.floor(seconds / 60);
     const secs = Math.floor(seconds % 60);
     return `${minutes < 10 ? '0' : ''}${minutes}:${secs < 10 ? '0' : ''}${secs}`;
   };
 
   useEffect(() => {
-    if (audio) {
+    if (!paused) {
       audio.play();
-      setPaused(false);
       const interval = setInterval(() => {
         setAudioPosition((audio.currentTime / audio.duration) * 100);
       }, 1000);
@@ -132,34 +142,55 @@ const App = () => {
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
     <div className="mb-auto">
       <input type="text" placeholder='What do you want to play?' className="bg-third p-3 rounded-full text-sm pr-10 mb-5 w-fit text-white focus:outline-none focus:ring-white focus:ring-2 m-3" onKeyDown={handleKeyDown} />
+
       <div className="text-grays bg-primary rounded-lg mx-3 mb-20">
         {tracks.map((track: any, index: number) => {
           return (
-            <div key={track.id} className="flex items-center p-3 rounded-xl hover:bg-third" onClick={async () => getAudioData(track.name + " by " + track.artists[0].name)}>
-              <div className="mr-5 font-semibold">{index + 1}</div>
-              <img src={track.album.images[0].url} alt={track.name} className="w-16 h-16 rounded-lg" />
-              <div className="ml-3">
+            <div key={track.id} className="flex items-center p-3 rounded-xl hover:bg-third" onClick={async () => {
+              getAudioData(track.name + " by " + track.artists[0].name);
+              setTrack({
+                name: track.name,
+                artists: track.artists[0].name,
+                album: track.album,
+                thumbnail: track.album.images[0].url,
+              });
+
+            }}>
+              <div className="font-semibold w-5 mr-3">{index + 1}</div>
+              <img src={track.album.images[0].url} alt={track.name} className="w-14 h-14 rounded-lg" />
+              <div className="ml-3 text-sm">
                 <p className="text-white">{track.name}</p>
-                <p className="text-grays-400">{track.artists[0].name}</p>
+                <p className="text-grays">{track.artists[0].name}</p>
               </div>
             </div>
           )
         })}
       </div>
     </div>
-    <footer className="text-white h-fit w-full bg-background flex flex-col font-sans mt-auto p-3 fixed bottom-0">
-      <div className="w-full items-center justify-center flex mb-3">
-      {paused ? 
-        <FaPlayCircle size={40} color='#FFFFFF' className="hover:cursor-pointer" onClick={handlePlayPause} /> :
-        <FaPauseCircle size={40} color='#FFFFFF' className="hover:cursor-pointer" onClick={handlePlayPause} />
-      }
-      </div>
-      <div className="justify-center flex items-center">
-        <div className="text-sm mx-3">{formatTime(audio.currentTime)}</div>
-        <div className="w-[40%] bg-gray-200 rounded-full h-1 dark:bg-gray-700 items-center">
-          <div className="bg-white h-1 rounded-full dark:bg-white" style={{width: `${audioPosition}%`}}></div>
+    <footer className="text-white h-fit w-full bg-background flex font-sans mt-auto p-3 fixed bottom-0" onKeyDown={handleKeyDown}>
+      {track && (
+        <div className="flex items-center flex-grow text-sm w-auto absolute">
+          <img src={track.thumbnail} alt={track.name} className="w-16 h-16 rounded-lg" />
+          <div className="ml-3">
+            <p className="text-white">{track.name}</p>
+            <p className="text-grays">{track.artists}</p>
+          </div>
         </div>
-        <div className="text-sm mx-3">{formatTime(audio.duration)}</div>
+      )}
+      <div className="flex flex-col w-full">
+        <div className="w-full items-center justify-center flex mb-1">
+        {paused ?
+          <FaPlayCircle size={40} color='#FFFFFF' className="hover:cursor-pointer" onClick={handlePlayPause} /> :
+          <FaPauseCircle size={40} color='#FFFFFF' className="hover:cursor-pointer" onClick={handlePlayPause} />
+        }
+        </div>
+        <div className="justify-center flex items-center">
+          <div className="text-sm mx-3 text-grays">{formatTime(audio.currentTime)}</div>
+          <div className="w-[30%] bg-gray-200 rounded-full h-1 dark:bg-gray-700 items-center">
+            <div className="bg-white h-1 rounded-full dark:bg-white" style={{width: `${audioPosition}%`}}></div>
+          </div>
+          <div className="text-sm mx-3 text-grays">{formatTime(audio.duration)}</div>
+        </div>
       </div>
     </footer>
     </main>
